a20a286668736a222eb77a5e398f6dc016793378
rodrigonahid <rodrigonahid1@gmail.com>
2021-11-09 19:34:36 -0300

    error handling and tags migration

---
+++ src/database/migrations/1636497258043-CreateTags.ts
@@ -0,0 +1,11 @@
+import {MigrationInterface, QueryRunner} from "typeorm";
+
+export class CreateTags1636497258043 implements MigrationInterface {
+
+    public async up(queryRunner: QueryRunner): Promise<void> {
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+    }
+
+}
